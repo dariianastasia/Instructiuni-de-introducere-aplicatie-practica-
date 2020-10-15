23d751809e6826c10cb81c07547e6cc44bab342d
dariianastasia <71002237+dariianastasia@users.noreply.github.com>
2020-10-15 14:56:56 +0300

    Add files via upload

---
+++ exercitiul.7.py
@@ -0,0 +1,6 @@
+"""Marina vrea sa verifice daca greutatea si inatimea ei corepunde virstei pe care o are.Ea  gasit intr-o carte urmatoarele formule de calcul ale greutatii si inaltimiiunui copil , v fiind virsta:greutatea=2*v+8(in kg), inaltimea= 5*v+80(in cm).Realizati un program care sa citeasca virsta unui copil si sa afiseze greutatea si inaltimea ideala, folosind aceste formule."""
+v=int(input("Introdu virsta sa:"))
+g=2*v+8
+i=5*v+80
+print("Greutatea voastra este", g,"kg")
+print("Inaltimea voastra este", i ,"cm")

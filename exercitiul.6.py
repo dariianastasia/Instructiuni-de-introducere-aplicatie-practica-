7d120eaf7d7886f09982ec12942b85c608233ae7
dariianastasia <71002237+dariianastasia@users.noreply.github.com>
2020-10-15 14:39:10 +0300

    Add files via upload

---
+++ exercitiul.6.py
@@ -0,0 +1,6 @@
+"""Doi copii au prinit acelasi numar de mere. Introdcind de la tastatura numarul de mere primite , afisati cite mere are fiecare copil dupa ce primul copil maninca un mar si da unul celuilalt copil."""
+n=int(input("Introdu numarul de mere:"))
+c1=n-2
+c2=n+1
+print("Prinul copil are",c1,"mere" )
+print("Al doilea copi; are",c2,"mere")

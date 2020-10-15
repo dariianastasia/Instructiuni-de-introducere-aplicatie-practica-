39dbb104a116ac0cb53f10465a77e3621c7fbb88
dariianastasia <71002237+dariianastasia@users.noreply.github.com>
2020-10-15 15:48:09 +0300

    Add files via upload

---
+++ exercitiul.12.py
@@ -0,0 +1,14 @@
+"""Intr-o gospodarie sunt 4 gaini.Introduceti in calculator prin variabilele a,b,c,d numarul de oua pe care-l da fiecare gina intr-o zi.Afiseaza cate oua se obtin intr-o saptamina"""
+a=int(input("introdu a:"))
+b=int(input("introdu b:"))
+c=int(input("introdu c:"))
+d=int(input("introdu d;"))
+sa=a*7
+sb=b*7
+sc=c*7
+sd=d*7
+print("prima gaina face intr-o saptamina ",sa,"oua")
+print(" a doua gaina face iontr-o saptamina",sb,"oua")
+print("a treia gaina face intr-o saptamina",sc,"oua")
+print(" a patra gaina face intr-o saptamina ",sd,"oua")
+

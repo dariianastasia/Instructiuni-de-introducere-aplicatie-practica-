6904140a24df71ee4e2ac539b742a6d2917a33d1
dariianastasia <71002237+dariianastasia@users.noreply.github.com>
2020-10-15 15:20:37 +0300

    Add files via upload

---
+++ exercitiul.9.py
@@ -0,0 +1,7 @@
+"""Date trei numere, sa se calculeze toate sumele posibile de cite doua numere.Afisarea sa cuprinda si termenii sumei , nu numai valoarea ei."""
+a=int(input("Introduceti a:"))
+b=int(input("Introduceti b:"))
+c=int(input("Introduceti c:"))
+print(a,"+",b,"=",a+b)
+print(a,"+",c,"=",a+c)
+print(b,"+",c,"=",b+c)
